6edad4b72034a91396334fa207a5aaae600ebc8a
lsr <imlimr1232@gmail.com>
2020-07-19 15:55:43 +0900

    add login, token

--- model/user.js
+++ model/user.js
@@ -1,5 +1,7 @@
 // 1
 const mongoose = require("mongoose");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // 2
 const userSchema = mongoose.Schema(
@@ -24,6 +26,28 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  try {
+    console.log("entered");
+
+    const avatar = await gravatar.url(this.email, {
+      s: "200",
+      r: "pg",
+      d: "mm",
+    });
+    this.avatar = avatar;
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+
+    console.log("exited");
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 // 3
 module.exports = mongoose.model("user", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+
 const userModel = require("../model/user");
 
 // @route GET http://localhost:5000/user/test
@@ -17,46 +18,79 @@ const userModel = require("../model/user");
 
 router.post("/register", (req, res) => {
   // email 유무 체크 -> password 암호화 -> DB 저장
+
+  const { name, email, password } = req.body;
+
   userModel
-    .findOne({ email: req.body.email })
+    .findOne({ email })
     .then((user) => {
       if (user) {
         return res.json({
           message: "email already exists",
         });
       } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: "200", // size
-          r: "pg", // rating
-          d: "mm", // Default
+        const newUser = new userModel({
+          name,
+          email,
+          password,
         });
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.json({
-              message: err.message,
+
+        newUser
+          .save()
+          .then((result) => {
+            res.json({
+              message: "save user",
+              userInfo: {
+                id: result._id,
+                name: result.name,
+                email: result.email,
+                avatar: result.avatar,
+                password: result.password,
+                date: {
+                  createdAt: result.createdAt,
+                  updatedAt: result.updatedAt,
+                },
+              },
             });
-          } else {
-            const user = new userModel({
-              name: req.body.name,
-              email: req.body.email,
-              password: hash,
-              avatar: avatar,
+          })
+          .catch((err) => {
+            res.json({
+              error: err.message,
             });
-            user
-              .save()
-              .then((result) => {
-                res.json({
-                  message: "save user",
-                  userInfo: result,
-                });
-              })
-              .catch((err) => {
-                res.json({
-                  error: err.message,
-                });
-              });
-          }
-        });
+          });
+
+        // const avatar = gravatar.url(req.body.email, {
+        //   s: "200", // size
+        //   r: "pg", // rating
+        //   d: "mm", // Default
+        // });
+        // bcrypt.hash(req.body.password, 10, (err, hash) => {
+        //   if (err) {
+        //     return res.json({
+        //       message: err.message,
+        //     });
+        //   } else {
+        //     const user = new userModel({
+        //       name: req.body.name,
+        //       email: req.body.email,
+        //       password: hash,
+        //       avatar: avatar,
+        //     });
+        //     user
+        //       .save()
+        //       .then((result) => {
+        //         res.json({
+        //           message: "save user",
+        //           userInfo: result,
+        //         });
+        //       })
+        //       .catch((err) => {
+        //         res.json({
+        //           error: err.message,
+        //         });
+        //       });
+        //   }
+        // });
       }
     })
     .catch((err) => {
@@ -70,7 +104,48 @@ router.post("/register", (req, res) => {
 // @desc Login user / returning jwt
 // @access Public
 
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  // email 체크 -> password 체크 -> returning jwt
+
+  const { email, password } = req.body;
+  userModel
+    .findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.json({
+          message: "No email",
+        });
+      } else {
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err || result === false) {
+            return res.json({
+              message: "password incorrect",
+            });
+          } else {
+            const payload = {
+              id: user._id,
+              email: user.email,
+              name: user.name,
+              avatar: user.avatar,
+            };
+
+            const token = jwt.sign(payload, process.env.SECRET, {
+              expiresIn: 36000,
+            });
+            res.json({
+              message: "successful login",
+              tokenInfo: token,
+            });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.json({
+        error: err.message,
+      });
+    });
+});
 
 // @route GET /current
 // @desc Current user
