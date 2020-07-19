255563bdcfba3a71f72ff28f5315a3b199429f1a
lsr <imlimr1232@gmail.com>
2020-07-19 14:33:02 +0900

    add database

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
-dotenv.config;
+dotenv.config();
 
 const app = express();
+
+// DB connect
+require("./config/database.js");
 
 // 미들웨어 설정
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 7000;
 
-app.listen(PORT, console.log("sever started..."));
+app.listen(PORT, console.log(`sever started at ${PORT}`));
