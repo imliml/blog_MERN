7d2c8775f5ab0583aaf79cd15524b6f8d0427459
lsr <imlimr1232@gmail.com>
2020-08-08 15:21:42 +0900

    add post model

---
+++ model/post.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    likes: [],
+    comments: [],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("post", postSchema);
