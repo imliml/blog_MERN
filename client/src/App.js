1d8c9c35234e9efa71fbec44df451c9ae56e185e
lsr <imlimr1232@gmail.com>
2020-08-15 15:28:35 +0900

    add client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>임성락</h1>
+      <h2>안양</h2>
+    </div>
+  );
+}
+
+export default App;
