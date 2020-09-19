4aabe53110dd83c4a320867998b30cbb4c2cb0a3
lsr <imlimr1232@gmail.com>
2020-09-19 11:34:52 +0900

    Add Login

--- client/src/layout/auth/Login.js
+++ client/src/layout/auth/Login.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const loginData = {
+      email,
+      password,
+    };
+    console.log(loginData);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Login</h1>
+            <p className="lead text-center">Login user</p>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  className="form-group"
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  className="form-group"
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
