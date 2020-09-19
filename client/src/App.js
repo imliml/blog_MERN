27e4e0a1925e25cd5370f207cf1c3fdeffe38d66
lsr <imlimr1232@gmail.com>
2020-09-19 09:49:33 +0900

    Reinstall client CRA and client project init

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>임성락</h1>
-      <h2>안양</h2>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
