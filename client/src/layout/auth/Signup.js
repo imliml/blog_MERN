3056ce199baf5e3897c8a2b0c0489553e2aec739
lsr <imlimr1232@gmail.com>
2020-09-19 11:20:48 +0900

    Add Signup

--- client/src/layout/auth/Signup.js
+++ client/src/layout/auth/Signup.js
@@ -1,9 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const userData = {
+      name,
+      email,
+      password,
+      password2,
+    };
+    console.log(userData);
+  };
+
   return (
-    <div>
-      <h1>Sign up</h1>
+    <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Sign Up</h1>
+            <p className="lead text-center">Create your DevConnector account</p>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Name"
+                  name="name"
+                  value={name}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Confirm Password"
+                  name="password2"
+                  value={password2}
+                  onChange={onChange}
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+            <br />
+            <p className="my-1">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
